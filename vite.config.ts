a2c78b456ddbd697dc02a734554cb3adb4dcff80
Truong Quang Vinh <vinhtruong2k4@gmail.com>
2025-09-16 14:36:15 +0700

    remove  assets core package

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 
-import { assetpackPlugin } from "./scripts/assetpack-vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [assetpackPlugin()],
+  plugins: [],
   server: {
     port: 8080,
     open: true,
